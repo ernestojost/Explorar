d363f6a45921760c1aed30f8563309feed652bc0
ernestojost <ernestojost@hotmail.com>
2020-04-30 16:36:50 -0300

    Avances en Menpu principal
    - Agregado fondo negro al pasar por arriba de opciones
    - Agregado texto dentro de ese fondo negro
    - Agregado puntero al pasar por encima de opciones

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent implements OnInit{
 
-  constructor() { }
+  public id_mostrarContenidoOpcionAventura;
+  public id_mostrarContenidoOpcionRomance;
+  public id_mostrarContenidoOpcionSolYPlaya;
+
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
+
 
     /* Crear y capturar elementos */
 
@@ -17,25 +24,23 @@ export class MenuComponent implements OnInit {
     var barra_progreso = document.getElementById("barra-progreso");  
     var pantalla_carga = document.getElementById("pantalla_carga");
     var menu_principal = document.getElementById("menu_principal"); 
+    var id = setInterval(frame, 10);
     var width = 0;
     var opacity = 140;
-    var id = setInterval(frame, 10);
     var contador = 1;
 
     /* Menú principal */
     var id_movimiento_imagenes;
     var opcionAventura = document.getElementById("aventura");
-    var opcionRomantico = document.getElementById("romantico");
+    var opcionRomance = document.getElementById("romance");
     var opcionSolYPlaya = document.getElementById("solyplaya");
     var imagenAventura = document.getElementById("imagenAventura");
-    var imagenRomantico = document.getElementById("imagenRomantico");
+    var imagenRomance = document.getElementById("imagenRomance");
     var imagenSolYPlaya = document.getElementById("imagenSolYPlaya");
     var scale = 2;
 
     /* FIN Crear y capturar elementos */
-
-
-
+    
     /* Funcionamiento barra de progreso de la pantalla de carga */
     function frame() {
       if (width >= 100) {
@@ -71,10 +76,10 @@ export class MenuComponent implements OnInit {
         clearInterval(id_movimiento_imagenes);
       }else{
         opcionAventura.style.opacity = opacity + '%';
-        opcionRomantico.style.opacity = opacity + '%';
+        opcionRomance.style.opacity = opacity + '%';
         opcionSolYPlaya.style.opacity = opacity + '%';
         imagenAventura.style.transform = "scale("+scale+")";
-        imagenRomantico.style.transform = "scale("+scale+")";
+        imagenRomance.style.transform = "scale("+scale+")";
         imagenSolYPlaya.style.transform = "scale("+scale+")";
         if(scale > 1.01){
           scale = scale - 0.005;
@@ -85,15 +90,119 @@ export class MenuComponent implements OnInit {
 
     /* FIN Carga de menú principal */
 
-    /* Al pasar ratón por encima de opciones */
-    
-    function overMostrarOpcion(opcion){
-      console.log("sdasdsa")
-      var id_mostrarContenidoOpcion;
-      var fondoNegro = document.getElementById("fondoNegroAventura");
-      fondoNegro.style.opacity = '100%';
-    }
-
   }
+
+
+
+  /* Al pasar ratón por encima de opciones */
+  overMostrarOpcionAventura(){
+    if(document.getElementById("imagenAventura").style.transform == "scale("+1.005+")"){
+      document.getElementById("aventura").style.cursor = "pointer";
+      var fondoNegro = document.getElementById("fondoNegroAventura");
+      if(fondoNegro.style.opacity == ''){
+        fondoNegro.style.opacity = 0 + '%';
+      }
+      clearInterval(this.id_mostrarContenidoOpcionAventura);
+      this.id_mostrarContenidoOpcionAventura = setInterval(movimientoContenidoAventura, 10);
+      var opacity = 0;
+      function movimientoContenidoAventura(){
+        if(opacity > 100){
+          clearInterval(this.id_mostrarContenidoOpcionAventura);
+        }else{
+          fondoNegro.style.opacity = opacity + '%';
+          opacity = opacity + 4;
+        }
+      }
+    }
+  }
+
+  leaveMostrarOpcionAventura(){
+    clearInterval(this.id_mostrarContenidoOpcionAventura);
+    var fondoNegro = document.getElementById("fondoNegroAventura");
+    this.id_mostrarContenidoOpcionAventura = setInterval(movimientoContenidoAventura, 10);
+    var opacity = Math.round(parseFloat(fondoNegro.style.opacity)*100);
+    function movimientoContenidoAventura(){
+      if(opacity < 0){
+        clearInterval(this.id_mostrarContenidoOpcionAventura);
+      }else{
+        fondoNegro.style.opacity = opacity + '%';
+        opacity = opacity - 4;
+      }
+    }
+  }
+
+  overMostrarOpcionRomance(){
+    if(document.getElementById("imagenRomance").style.transform == "scale("+1.005+")"){
+      document.getElementById("romance").style.cursor = "pointer";
+      var fondoNegro = document.getElementById("fondoNegroRomance");
+      if(fondoNegro.style.opacity == ''){
+        fondoNegro.style.opacity = 0 + '%';
+      }
+      clearInterval(this.id_mostrarContenidoOpcionRomance);
+      this.id_mostrarContenidoOpcionRomance = setInterval(movimientoContenidoRomance, 10);
+      var opacity = 0;
+      function movimientoContenidoRomance(){
+        if(opacity > 100){
+          clearInterval(this.id_mostrarContenidoOpcionRomance);
+        }else{
+          fondoNegro.style.opacity = opacity + '%';
+          opacity = opacity + 4;
+        }
+      }
+    }
+  }
+
+  leaveMostrarOpcionRomance(){
+    clearInterval(this.id_mostrarContenidoOpcionRomance);
+    var fondoNegro = document.getElementById("fondoNegroRomance");
+    this.id_mostrarContenidoOpcionRomance = setInterval(movimientoContenidoRomance, 10);
+    var opacity = Math.round(parseFloat(fondoNegro.style.opacity)*100);
+    function movimientoContenidoRomance(){
+      if(opacity < 0){
+        clearInterval(this.id_mostrarContenidoOpcionRomance);
+      }else{
+        fondoNegro.style.opacity = opacity + '%';
+        opacity = opacity - 4;
+      }
+    }
+  }
+
+  overMostrarOpcionSolYPlaya(){
+    if(document.getElementById("imagenSolYPlaya").style.transform == "scale("+1.005+")"){
+      document.getElementById("solyplaya").style.cursor = "pointer";
+      var fondoNegro = document.getElementById("fondoNegroSolYPlaya");
+      if(fondoNegro.style.opacity == ''){
+        fondoNegro.style.opacity = 0 + '%';
+      }
+      clearInterval(this.id_mostrarContenidoOpcionSolYPlaya);
+      this.id_mostrarContenidoOpcionSolYPlaya = setInterval(movimientoContenidoSolYPlaya, 10);
+      var opacity = 0;
+      function movimientoContenidoSolYPlaya(){
+        if(opacity > 100){
+          clearInterval(this.id_mostrarContenidoOpcionSolYPlaya);
+        }else{
+          fondoNegro.style.opacity = opacity + '%';
+          opacity = opacity + 4;
+        }
+      }
+    }
+  }
+
+  leaveMostrarOpcionSolYPlaya(){
+    clearInterval(this.id_mostrarContenidoOpcionSolYPlaya);
+    var fondoNegro = document.getElementById("fondoNegroSolYPlaya");
+    this.id_mostrarContenidoOpcionSolYPlaya = setInterval(movimientoContenidoSolYPlaya, 10);
+    var opacity = Math.round(parseFloat(fondoNegro.style.opacity)*100);
+    function movimientoContenidoSolYPlaya(){
+      if(opacity < 0){
+        clearInterval(this.id_mostrarContenidoOpcionSolYPlaya);
+      }else{
+        fondoNegro.style.opacity = opacity + '%';
+        opacity = opacity - 4;
+      }
+    }
+  }
+
+  /* FIN Al pasar ratón por encima de opciones */
 
 }
