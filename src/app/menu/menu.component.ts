ea5e2a79b39fe0470ffbc4f92b30096cc25fa70f
ernestojost <ernestojost@hotmail.com>
2020-04-30 11:14:50 -0300

    Pantalla de carga

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -11,21 +11,44 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
 
-    // Funcionamiento barra de progreso
-    var barra_progreso = document.getElementById("barra-progreso");   
+    /* Crear y capturar elementos */
+
+    /* Pantalla de carga */
+    var barra_progreso = document.getElementById("barra-progreso");  
+    var pantalla_carga = document.getElementById("pantalla_carga");
+    var menu_principal = document.getElementById("menu_principal"); 
     var width = 0;
     var opacity = 140;
-    var contenido = document.getElementById("contenido");
     var id = setInterval(frame, 10);
     var contador = 1;
+
+    /* Menú principal */
+    var id_movimiento_imagenes;
+    var opcionAventura = document.getElementById("aventura");
+    var opcionRomantico = document.getElementById("romantico");
+    var opcionSolYPlaya = document.getElementById("solyplaya");
+    var imagenAventura = document.getElementById("imagenAventura");
+    var imagenRomantico = document.getElementById("imagenRomantico");
+    var imagenSolYPlaya = document.getElementById("imagenSolYPlaya");
+    var scale = 2;
+
+    /* FIN Crear y capturar elementos */
+
+
+
+    /* Funcionamiento barra de progreso de la pantalla de carga */
     function frame() {
       if (width >= 100) {
         if (opacity <= 100){
-          contenido.style.opacity = opacity + '%';
+          pantalla_carga.style.opacity = opacity + '%';
         }
         opacity--;
         if (opacity < 0){
           clearInterval(id);
+          pantalla_carga.style.display = "none";
+          menu_principal.style.display = "flex";
+          opacity = 0;
+          id_movimiento_imagenes = setInterval(movimientoImagenes, 10);
         }
       } else {
         width = width + contador/100;
@@ -37,6 +60,40 @@ export class MenuComponent implements OnInit {
         barra_progreso.style.width = width + '%'; 
       }
     }
+
+    /* FIN Funcionamiento barra de progreso de la pantalla de carga */
+
+
+
+    /* Carga de menú principal */
+    function movimientoImagenes(){
+      if(opacity > 100){
+        clearInterval(id_movimiento_imagenes);
+      }else{
+        opcionAventura.style.opacity = opacity + '%';
+        opcionRomantico.style.opacity = opacity + '%';
+        opcionSolYPlaya.style.opacity = opacity + '%';
+        imagenAventura.style.transform = "scale("+scale+")";
+        imagenRomantico.style.transform = "scale("+scale+")";
+        imagenSolYPlaya.style.transform = "scale("+scale+")";
+        if(scale > 1.01){
+          scale = scale - 0.005;
+        }
+        opacity = opacity + 0.5;
+      }
+    }
+
+    /* FIN Carga de menú principal */
+
+    /* Al pasar ratón por encima de opciones */
+    
+    function overMostrarOpcion(opcion){
+      console.log("sdasdsa")
+      var id_mostrarContenidoOpcion;
+      var fondoNegro = document.getElementById("fondoNegroAventura");
+      fondoNegro.style.opacity = '100%';
+    }
+
   }
 
 }
